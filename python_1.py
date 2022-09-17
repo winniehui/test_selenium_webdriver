a095cc0f754204731599669a58cf9a00ef4582b8
keith smart pigpig <wgoodjob@163.com>
2022-09-18 06:30:01 +0800

    modified python pop window

--- python_1.py
+++ python_1.py
@@ -1,7 +1,20 @@
 from selenium import webdriver
 import time
 
+# 启动浏览器，打开页面
 dr = webdriver.Chrome()
 dr.get("https://www.python.org/")
 
-driver.quit()
+# 修改标题
+time.sleep(1)
+JS1 = "document.title='修改弹窗标题：猪仔茂茂系叻叻猪';"
+dr.execute_script(JS1)
+
+# 弹窗标题
+time.sleep(1)
+JS2=r"alert($(document).attr('title'));"
+dr.execute_script(JS2)
+time.sleep(3)
+
+# 退出
+dr.quit()
